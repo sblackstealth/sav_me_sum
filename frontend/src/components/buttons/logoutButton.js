fe13074decdfb7fa697828fc680de42784d6d8e5
shaedonBlackman <sblackman89@gmail.com>
2018-06-03 18:20:12 -0400

    added buttons to homepage

---
+++ frontend/src/components/buttons/logoutButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LogoutUserButton extends Component {
+  render() {
+    return (
+      <div className="logoutUserButton">
+       <button className="logoutButton">>logout</button>
+      </div>
+    );
+  }
+}
+
+export default LogoutUserButton;
---
+++ frontend/src/components/buttons/registerButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RegisterUserButton extends Component {
+  render() {
+    return (
+      <div className="registerUserButton">
+         <button className="registerButton">>register</button>
+      </div>
+    );
+  }
+}
+
+export default RegisterUserButton;
